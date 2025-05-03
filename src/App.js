6afe650d9a6391508f80f9d365cfa06b53360744
Sargsyan001-s <mariamsargsan302.gmail.com>
2025-05-03 14:45:59 +0300

    первый коммит

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function Button () {
+  return (
+    <h1>
+      <button class="btn">Кнопка</button>
+    </h1>
+  );
+}
+
+export default function myApp() {
+  return (
+    <div>
+      <h1>
+        Lmao
+      </h1>
+      <Button/>;
+    </div>
+  )
+}
